3c02a83cc74cd4b5bae9225d29001437d0e4b1db
Asad Alwase <asadalwase@gmail.com>
2025-09-14 15:32:46 +0530

    code changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import pickle
-import numpy
+import numpy as np
 app=Flask(__name__)
 
 # Load the trained pipeline (preprocessing + model together)
